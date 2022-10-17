7da0f65e654af2dc67a386678527ce8b233be11d
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-17 21:54:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const name = "Online Chat App"
+  const description = "people can easily chat online with any stranger"
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">{name}</h1>
+      <p data-ns-test="project-description">{description}</p>
+    </div>
   )
 }
 
